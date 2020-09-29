c6b32bc1053492c2dd1552c3ff30e685944ca59f
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-09-29 21:19:01 +0300

    Added main app logic

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,3 +1,10 @@
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
+
+public class Example{
+
+    public static void main(String[] args){
+        SpringApplication.run(Example.class, args);
+    }
+}
