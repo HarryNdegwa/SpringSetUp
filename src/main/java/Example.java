6d0d3688402f555d4bc3b3cb69e92b19ef88ad9c
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-09-29 21:22:22 +0300

    Added routing to home root

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -2,7 +2,15 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
 
+
+@RestController
+@EnableAutoConfiguration
 public class Example{
+
+    @RequestMapping("/")
+    String hello(){
+        return "Helloo World";
+    }
 
     public static void main(String[] args){
         SpringApplication.run(Example.class, args);
